8d49db5a47b3837135b5b5bf1d2bef17b986c300
Grace Kim <gracekim@ibm.com@Graces-MacBook-Pro.local>
2020-07-01 11:49:45 -0700

    try again connect web app and bot

--- app.py
+++ app.py
@@ -1,70 +1,7 @@
-from flask import Flask, render_template
-from config import *
-import bot as b
-import socket
-import csv
-import random
-import sys 
-#import os
-#import sys
-
-# def readbot_file():
-#   with open(bot.py) as f:
-#       read_data = f.read() 
-
-app = Flask(__name__)
-
-
-b.loadQuestions()
-
-server = socket.socket()
-server.connect((HOST, PORT))
-server.send(bytes("PASS " + PASS + "\r\n", "utf-8"))
-server.send(bytes("NICK " + NICK + "\r\n", "utf-8"))
-server.send(bytes("JOIN #" + CHANNEL + "\r\n", "utf-8"))
-b.joinChat()
-
-b.chooseQuestion()
-
-while True:
-    # print(server.recv(2048).decode("utf-8"))
-    line = server.recv(2048).decode("utf-8").split("\n")
-    if len(line) == 2:
-        line = line[0]
-        # print(line)
-        if "PING" in line:
-            b.pong(line)
-            continue
-        else:
-            user = b.getUser(line)
-            msg = b.getMsg(line)
-            print(user + ": " + msg)
-
-    if b.checkAnswer(msg):
-        b.sendMsg(user + " guessed the correct answer: " + b.answer)
-        b.chooseQuestion()
-
-    if b.game_over:
-        print("game over")
-        server.close()
-        sys.exit(0)
+from flask import Flask
+from bot import question, getQuestion
 
 # home route
 @app.route("/")
 def home():
-    # return "hi"
-    return b.question
-    # return render_template('index.html', name = 'Jane', gender = 'Female')
-
-# # serving form web page
-# @app.route("/my-form")
-# def form():
-#     return render_template('index.html')
-
-# # handling form data
-# @app.route('/form-handler', methods=['POST'])
-# def handle_data():
-    
-#     return "Request received successfully!"
-
-# app.run(debug = True)
+    return getQuestion()
--- bot.py
+++ bot.py
@@ -1,63 +1,84 @@
 from config import *
+from app import *
 import socket
 import csv
 import random
 import sys
-import app as a
+import time
 
+
+# list of all questions (question, answer) tuples
 questions = []
+# current question #, starting at 1
 q_num = 0
-question = ""
+# current question
+question = "foo"
+# current answer
 answer = ""
+# flag to close server and bot when game is over
 game_over = False
 
-def joinChat():
+# initialize socket connection
+def startSocket():
+    server = socket.socket()
+    server.connect((HOST, PORT))
+    server.send(bytes("PASS " + PASS + "\r\n", "utf-8"))
+    server.send(bytes("NICK " + NICK + "\r\n", "utf-8"))
+    server.send(bytes("JOIN #" + CHANNEL + "\r\n", "utf-8"))
+    return server
+
+# join chat
+def joinChat(server):
     while True:
-        line = a.server.recv(2048).decode("utf-8").split("/n").pop()
+        line = server.recv(2048).decode("utf-8").split("/n").pop()
         if "End of /NAMES list" in line:
-            sendMsg("triviabot has joined the chat")
+            sendMsg(server, "triviabot has joined the chat")
             return
 
+# parse csv file to load questions
 def loadQuestions():
     global questions
     with open(FILE, 'r') as csv_file: 
-        # creating a csv reader object 
+        # create a csv reader object 
         csv_reader = csv.reader(csv_file) 
-        # extracting each data row one by one 
+        # extract each data row one by one 
         for row in csv_reader: 
             questions.append((row[0], row[1]))
+        # remove header row
         questions = questions[1:]
-        # randomizes order of questions
+        # randomize order of questions
         random.shuffle(questions)
-        print("Questions loaded.")
         print("Total number of questions: %d" % len(questions)) 
         # print(questions)
 
+# choose next question from global questions list and update global question, answer, q_num
+# vars accordingly. releases question in chat
 def chooseQuestion():
     global questions, q_num, question, answer, game_over
+    # if all questions have been released, tells chat game is over and updates game_over flag
     if q_num == len(questions):
-        sendMsg("Those are all the questions we have! Thanks for playing :^)")
+        sendMsg(server, "Those are all the questions we have! Thanks for playing :^)")
         game_over = True
         return
     curr = questions[q_num]
     question = curr[0]
     answer = curr[1]
     q_num += 1
-    sendMsg("Question #%d of %d: %s" % (q_num, len(questions), question))    
+    # release question in chat
+    sendMsg(server, "Question #%d: %s" % (q_num, question))
+    app.run()
 
-# pongs server back to not timeout
-def pong(line):
-    a.server.send(bytes(line.replace("PING", "PONG") + "\r\n", "utf-8"))
+# pongs server back to avoid timing out
+def pong(server, line):
+    server.send(bytes(line.replace("PING", "PONG") + "\r\n", "utf-8"))
     print("ponged")
 
 # extracts username
 def getUser(line):
-    # print("user " + line.split("!")[0][1:])
     return line.split("!")[0][1:]
 
 # extracts message
 def getMsg(line):
-    # print("msg " + line.split(":")[2])
     return line.split(":")[2]
 
 # checks if message contains correct answer
@@ -67,49 +88,50 @@ def checkAnswer(msg):
         return False
     return True
 
-def sendMsg(msg):
-    a.server.send(bytes("PRIVMSG #" + CHANNEL + " :" + msg + "\r\n", "utf-8"))
+# sends message to chat
+def sendMsg(server, msg):
+    server.send(bytes("PRIVMSG #" + CHANNEL + " :" + msg + "\r\n", "utf-8"))
     print("Sent:", msg)
+    pass
 
+def getQuestion():
+    global question
+    return question
 
+def start(server):
+    while True:
+        # parse through data received from server
+        line = server.recv(2048).decode("utf-8").split("\n")
+        if len(line) == 2:
+            # line is individual chat messages or pings from server
+            line = line[0]
+            print(line)
+            # if server pings, pong back and receive next line
+            if "PING" in line:
+                pong(server, line)
+                continue
+            # if user sends a chat message, parse user and message
+            else:
+                user = getUser(line)
+                msg = getMsg(line)
+                print(user + ": " + msg)
+        # if message contains the answer, tell chat and choose new question
+        if checkAnswer(msg):
+            sendMsg(server, user + " guessed the correct answer: " + answer)
+            chooseQuestion()
+        # if all questions have been asnwered, quit bot
+        if game_over:
+            print("game over")
+            server.close()
+            sys.exit(0)
 
-
-# loadQuestions()
-
-# a.server = socket.socket()
-# a.server.connect((HOST, PORT))
-# a.server.send(bytes("PASS " + PASS + "\r\n", "utf-8"))
-# a.server.send(bytes("NICK " + NICK + "\r\n", "utf-8"))
-# a.server.send(bytes("JOIN #" + CHANNEL + "\r\n", "utf-8"))
-# joinChat()
-
-# chooseQuestion()
-
-# while True:
-#     # print(a.server.recv(2048).decode("utf-8"))
-#     line = a.server.recv(2048).decode("utf-8").split("\n")
-#     if len(line) == 2:
-#         line = line[0]
-#         # print(line)
-#         if "PING" in line:
-#             pong(line)
-#             continue
-#         else:
-#             user = getUser(line)
-#             msg = getMsg(line)
-#             print(user + ": " + msg)
-
-#     if checkAnswer(msg):
-#         sendMsg(user + " guessed the correct answer: " + answer)
-#         chooseQuestion()
-
-#     if game_over:
-#         print("game over")
-#         a.server.close()
-#         sys.exit(0)
-
-# def main():
-#     print "Hello World"
-
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    loadQuestions()
+    server = startSocket()
+    joinChat(server)
+    chooseQuestion()
+    # app.run()
+    start(server)
+    time.sleep(5)
+    
+    
